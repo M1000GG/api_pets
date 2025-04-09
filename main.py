f5bcfb6281bfd1130a678237a8fad524ff70ced7
M1000GG <mgutierrez107696@umanizales.edu.co>
2025-04-08 19:00:01 -0500

    ABB: Preorden, Inorden, Postorden

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from controller.abb_controller import ABBController
+import uvicorn
+
+app = FastAPI()
+
+controller = ABBController()
+app.include_router(controller.router, prefix="/api", tags=["pets"])
+
+# Ruta raíz
+@app.get("/", tags=["root"])
+def read_root():
+    return {"message": "Bienvenido a la API de gestión de mascotas"}
+
+# Iniciar la aplicación cuando se ejecuta directamente
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
